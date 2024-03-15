440f7d8c5897ec235f5d841d2bd0c5e5239ad71e
Shabrina Maharani <130067945+Maharanish@users.noreply.github.com>
2024-03-15 17:42:03 +0700

    src : fix dnc method

--- src/beziercurve.py
+++ src/beziercurve.py
@@ -1,52 +1,74 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
 class BezierCurve:
     def __init__(self):
         self.curve_points = []
         self.control_points = []
 
+    def calculate_bezier_three_point(self, control_points, iterations):
+        if len(control_points) == 3 and iterations == 1:
+            # Basis case: jika ada tiga titik dan iterasi adalah 1, hitung midpoint tiga titik
+            midpoint = self.midpoint_of_three(*control_points)
+            return [midpoint]
+        elif len(control_points) == 3 and iterations > 1:
+            # Rekursi: jika ada tiga titik dan iterasi lebih dari 1
+            midpoint_a = self.midpoint(control_points[0], control_points[1])
+            midpoint_b = self.midpoint(control_points[1], control_points[2])
+            
+            for i in range(iterations):
+                midpoint_of_three = self.midpoint_of_three(control_points[0], control_points[1], control_points[2])
+                new_control_points1 = [control_points[0], midpoint_a, midpoint_of_three]
+                new_control_points2 = [midpoint_of_three, midpoint_b, control_points[2]]
+                curve_points1 = self.calculate_bezier_three_point(new_control_points1, iterations - 1)
+                curve_points2 = self.calculate_bezier_three_point(new_control_points2, iterations - 1)
+            return curve_points1+curve_points2
+        else:
+            return []
+        
+
     def make_bezier(self, *control_points, iterations):
         self.curve_points.clear()
         self.control_points = list(control_points)
-        self.calculate_bezier(control_points, 0, iterations)
 
-    def calculate_bezier(self, control_points, current_iteration, iterations):
-        new_control_points = []
-        if current_iteration < iterations:
-            if current_iteration == 0:
-                # Hitung midpoint antara P0 dan P1 (Q0)
-                midpoint_Q0 = self.mid_point(control_points[0], control_points[1])
-                self.curve_points.append(midpoint_Q0)
-                # Hitung midpoint antara P1 dan P2 (Q1)
-                midpoint_Q1 = self.mid_point(control_points[1], control_points[2])
-                self.curve_points.append(midpoint_Q1)
-                # Hitung midpoint antara Q0 dan Q1 (R0)
-                midpoint_R0 = self.mid_point(midpoint_Q0, midpoint_Q1)
-                self.curve_points.append(midpoint_R0)
-                new_control_points = [control_points[0],midpoint_Q0, midpoint_R0,control_points[1],midpoint_Q1]  
-            else:
-                for _ in range(iterations - 1):  
-                    new_points = [control_points[0]]  # Titik awal
-                    # Iterasi untuk menambahkan midpoint dan titik kontrol berikutnya
-                    for i in range(len(control_points) - 1):
-                        # Hitung midpoint antara titik kontrol i dan i+1
-                        midpoint = self.mid_point(control_points[i], control_points[i + 1])
-                        new_points.extend([midpoint, control_points[i + 1]])  # Tambahkan midpoint dan titik kontrol berikutnya
-                    control_points = new_points  # Update titik kontrol
-                new_control_points.extend(control_points)
-            current_iteration += 1
-            self.calculate_bezier(new_control_points, current_iteration, iterations)
+        iterations1 = self.midpoint_of_three(*control_points)
+        self.curve_points.append(iterations1)
+        #print(self.curve_points)
+
+        # Memulai rekursi untuk menghitung titik-titik kurva Bezier di iterasi yang lebih dari 1
+        if iterations>1 :
+            another_points_curve = self.calculate_bezier_three_point(control_points, iterations) 
+            self.curve_points.extend(another_points_curve.copy())
+        #print(self.curve_points)
+
+        
+        # Hubungkan titik kontrol pertama dengan titik kurva pertama
+        if len(self.curve_points) > 0:
+            self.curve_points.insert(0, self.control_points[0])
+        #print(self.curve_points)
+        
+        # Hubungkan titik kontrol terakhir dengan titik kurva terakhir
+        if len(self.curve_points) > 0:
+            self.curve_points.append(self.control_points[-1])
+        #print(self.curve_points)
+        self.curve_points=sorted(self.curve_points, key=lambda p: p[0])
+        #print(self.curve_points)
 
 
-
-    def mid_point(self, control_point1, control_point2):
-        return ((control_point1[0] + control_point2[0]) / 2, (control_point1[1] + control_point2[1]) / 2)
+    def midpoint(self, p1, p2):
+        return ((p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2)
+    
+    def midpoint_of_three(self, a, b, c):
+        # Menghitung midpoint dari tiga titik
+        return self.midpoint(self.midpoint(a, b), self.midpoint(b, c))
 
     def plot_curve(self):
+        # Plot titik kontrol
         plt.plot(*zip(*self.control_points), marker='o', color='red', label='Control Points')
+        
+        # Plot kurva Bezier
         plt.plot(*zip(*self.curve_points), linestyle='--', color='gray')
         plt.plot(*zip(*self.curve_points), marker='o', color='blue', label='Bezier Curve')
+        
         plt.legend()
         plt.title('Bezier Curve')
         plt.xlabel('X')
--- src/maindnc.py
+++ src/maindnc.py
@@ -3,7 +3,7 @@ from beziercurve import BezierCurve
 def main():
     bezier = BezierCurve()
     control_points = [(0,0), (2, 3), (5,1)]  
-    iterations = 1 
+    iterations = 3
     bezier.make_bezier(*control_points, iterations=iterations)
     bezier.plot_curve()
 
