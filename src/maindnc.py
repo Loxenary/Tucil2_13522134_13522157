26e2d6ab268120abd908fc11126f10fe19d5ab0f
Shabrina Maharani <130067945+Maharanish@users.noreply.github.com>
2024-03-15 22:35:58 +0700

    src\beziercurve.py : fix bug

--- src/maindnc.py
+++ src/maindnc.py
@@ -2,7 +2,7 @@ from beziercurve import BezierCurve
 
 def main():
     bezier = BezierCurve()
-    control_points = [(0.5,0), (2, 3), (5,1), (8,9)]  
+    control_points = [(100,200), (200, 300), (500,100)]  
     iterations = 3
     bezier.make_bezier(*control_points, iterations=iterations)
     bezier.plot_curve()
