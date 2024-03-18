9d9147b1db0bf7ca0fb8758025cb3da2b3e62973
Shabrina Maharani <130067945+Maharanish@users.noreply.github.com>
2024-03-18 19:45:13 +0700

    src : fix dnc dan brute force

--- src/maindnc.py
+++ src/maindnc.py
@@ -1,10 +1,37 @@
 from beziercurve import BezierCurve
+import time
+
+def input_control_points():
+    # Meminta input dari pengguna untuk titik kontrol
+    control_points = []
+    while True:
+        n = int(input("Masukkan banyak titik control points: "))
+        if n < 3:
+            print("Error: Minimum 3 control points required.")
+        else:
+            break
+    for i in range(n):
+        x = float(input(f"Masukkan koordinat x untuk control point {i+1}: "))
+        y = float(input(f"Masukkan koordinat y untuk control point {i+1}: "))
+        control_points.append((x, y))
+    return control_points
 
 def main():
     bezier = BezierCurve()
-    control_points = [(100,200), (200, 300), (500,100)]  
-    iterations = 3
+    
+    # Meminta input titik kontrol dan jumlah iterasi
+    control_points = input_control_points()
+    iterations = int(input("Masukkan jumlah iterasi: "))
+    
+    start_time = time.time()
+    # Menghasilkan kurva Bezier
     bezier.make_bezier(*control_points, iterations=iterations)
+    end_time = time.time()
+    
+    # Menghitung dan mencetak waktu yang dibutuhkan untuk pembuatan kurva
+    print("Time execution:", end_time - start_time, "seconds")
+    
+    # Menampilkan kurva Bezier
     bezier.plot_curve()
 
 if __name__ == "__main__":
